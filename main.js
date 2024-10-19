559e34f4b7ab1f25c770be1d882e5e06ffa542b1
Ankushkewat <ankushkewat78352@gmail.com>
2024-10-19 14:16:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log ("bhaiya")
+
+let btn= document.querySelector(".sub")
+
+let sus= document.querySelector(".sus")
+
+btn.addEventListener("click", change)
+;
+function change() {
+  sus.innerText = "SUCCESS "
+}
+
